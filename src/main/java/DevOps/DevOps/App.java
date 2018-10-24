305b6f44687e2f47b954b464fecc8fdea9ffa38f
aurelien.thomas <aurelient96@gmail.com>
2018-10-24 14:58:43 +0200

    archive

--- src/main/java/DevOps/DevOps/App.java
+++ src/main/java/DevOps/DevOps/App.java
@@ -8,4 +8,8 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
+
+	public String helloWorld() {
+		return "Hello World";
+	}
 }
--- src/test/java/DevOps/DevOps/AppTest.java
+++ src/test/java/DevOps/DevOps/AppTest.java
@@ -7,32 +7,48 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	App app;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
+
+	public void setUp() throws Exception {
+		this.app = new App();
+	}
+
+	public void tearDown() throws Exception {
+		this.app = null;
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+
+	public void testUnegaleUn() {
+		assertEquals(1, 1);
+	}
+
+	public void testHelloWorld() {
+		assertEquals(this.app.helloWorld(), "Hello World");
+	}
+
 }
